598a8e4b910540bf5b3e1da03198ed3e0af7d39d
lechos22 <63498252+lechos22@users.noreply.github.com>
2023-12-18 09:10:09 +0100

    Add Fizz Buzz endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 use std::error::Error;
 
-use actix_web::get;
+use actix_web::{get, web};
 
-#[get("/")]
-async fn index() -> &'static str {
-    "Hello world!"
+fn nth_entry(n: u64) -> String {
+    match n {
+        n if n % 15 == 0 => "Fizz Buzz".to_string(),
+        n if n % 3 == 0 => "Fizz".to_string(),
+        n if n % 5 == 0 => "Buzz".to_string(),
+        n => n.to_string(),
+    }
+}
+
+#[get("/nth/{n}")]
+async fn nth_entry_endpoint(path: web::Path<u64>) -> String {
+    nth_entry(path.into_inner())
 }
 
 #[actix_web::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    actix_web::HttpServer::new(|| actix_web::App::new().service(index))
+    actix_web::HttpServer::new(|| actix_web::App::new().service(nth_entry_endpoint))
         .bind("0.0.0.0:3000")?
         .run()
         .await?;
