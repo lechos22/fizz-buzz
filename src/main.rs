4512bd450a7a6fa94b1c7dea13ed1cbe42612cac
lechos22 <63498252+lechos22@users.noreply.github.com>
2023-12-18 15:28:54 +0100

    Create basic frontend

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 use std::error::Error;
 
-use actix_web::{get, web};
+use actix_web::{get, web, HttpResponse};
+
+fn index_html() -> &'static str {
+    include_str!("./index.html")
+}
+
+#[get("/")]
+async fn index() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(index_html())
+}
 
 fn nth_entry(n: u64) -> String {
     match n {
@@ -13,14 +24,18 @@ fn nth_entry(n: u64) -> String {
 
 #[get("/nth/{n}")]
 async fn nth_entry_endpoint(path: web::Path<u64>) -> String {
-    nth_entry(path.into_inner())
+    serde_json::to_string(&nth_entry(path.into_inner())).expect("Failed to serialize response")
 }
 
 #[actix_web::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    actix_web::HttpServer::new(|| actix_web::App::new().service(nth_entry_endpoint))
-        .bind("0.0.0.0:3000")?
-        .run()
-        .await?;
+    actix_web::HttpServer::new(|| {
+        actix_web::App::new()
+            .service(index)
+            .service(nth_entry_endpoint)
+    })
+    .bind("0.0.0.0:3000")?
+    .run()
+    .await?;
     Ok(())
 }
